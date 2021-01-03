b2552fd92db48f41495632a258b30d521670b142
Khalid Ishtiaque <52541004+istiaq27@users.noreply.github.com>
2021-01-03 22:28:24 +0600

    initial commit

---
+++ main.py
@@ -0,0 +1,152 @@
+import cv2
+import os
+import numpy as np
+import time
+
+stime = time.time()
+subjects = ["", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11", "s12", "s13", "s14", "s15", "s16",
+            "s17", "s18", "s19", "s20", "s21", "s22", "s23", "s24", "s25", "s26",
+            "s27", "s28", "s29", "s30", "s31", "s32", "s33", "s34", "s35", "s36", "s37", "s38"]
+
+
+def detect_face(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # face_cascade = cv2.CascadeClassifier('opencv-files/lbpcascade_frontalface.xml')
+    face_cascade = cv2.CascadeClassifier('opencv-files/haarcascade_frontalface_alt.xml')
+
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5);
+
+    if (len(faces) == 0):
+        return None, None
+
+    # extract the face area
+    (x, y, w, h) = faces[0]
+
+    # return only the face part of the image
+    return gray[y:y + w, x:x + h], faces[0]
+
+
+def prepare_training_data(data_folder_path):
+    # get the directories (one directory for each subject) in data folder
+    dirs = os.listdir(data_folder_path)
+
+    # list to hold all subject faces
+    faces = []
+    # list to hold labels for all subjects
+    labels = []
+
+    for dir_name in dirs:
+
+        if not dir_name.startswith("s"):
+            continue;
+
+        label = int(dir_name.replace("s", ""))
+
+        subject_dir_path = data_folder_path + "/" + dir_name
+
+        subject_images_names = os.listdir(subject_dir_path)
+
+        for image_name in subject_images_names:
+
+            # ignore system files like .DS_Store
+            if image_name.startswith("."):
+                continue;
+
+            image_path = subject_dir_path + "/" + image_name
+
+            # read image
+            image = cv2.imread(image_path)
+
+            # display an image window to show the image
+            cv2.imshow("Training on image...", image)
+            cv2.waitKey(100)
+
+            # detect face
+            face, rect = detect_face(image)
+
+            if face is not None:
+                # add face to list of faces
+                # face = cv2.resize(100, 100)
+                # faces.append(face)
+                faces.append(cv2.resize(face, (300, 300)))
+                # add label for this face
+                labels.append(label)
+
+    cv2.destroyAllWindows()
+    cv2.waitKey(1)
+    cv2.destroyAllWindows()
+
+    return faces, labels
+
+
+print("Preparing data...")
+faces, labels = prepare_training_data("training-data")
+print("Data prepared")
+
+# print total faces and labels
+print("Total faces: ", len(faces))
+print("Total labels: ", len(labels))
+
+#face_recognizer = cv2.face.EigenFaceRecognizer_create()
+face_recognizer = cv2.face.FisherFaceRecognizer_create()
+
+face_recognizer.train(faces, np.array(labels))
+
+
+def draw_rectangle(img, rect):
+    (x, y, w, h) = rect
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+
+# function to draw text on give image starting from
+# passed (x, y) coordinates.
+def draw_text(img, text, x, y):
+    cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
+
+
+def predict(test_img):
+    # make a copy of the image as we don't want to chang original image
+    img = test_img.copy()
+    # detect face from the image
+    face, rect = detect_face(img)
+    face = cv2.resize(face, (300, 300))
+
+    # predict the image using our face recognizer
+    label, confidence = face_recognizer.predict(face)
+    # get name of respective label returned by face recognizer
+    label_text = subjects[label]
+
+    # draw a rectangle around face detected
+    draw_rectangle(img, rect)
+    # draw name of predicted person
+    draw_text(img, label_text, rect[0], rect[1] - 5)
+
+    return img
+
+
+# In[10]:
+
+print("Predicting images...")
+
+# load test images
+test_img1 = cv2.imread("test-data/test1.jpg")
+test_img2 = cv2.imread("test-data/test2.jpg")
+test_img3 = cv2.imread("test-data/test3.jpg")
+
+# perform a prediction
+predicted_img1 = predict(test_img1)
+predicted_img2 = predict(test_img2)
+predicted_img3 = predict(test_img3)
+etime = time.time()
+print(f"time = {etime-stime}")
+print("Prediction complete")
+
+# display both images
+cv2.imshow(subjects[1], cv2.resize(predicted_img1, (300, 300)))
+cv2.imshow(subjects[2], cv2.resize(predicted_img2, (300, 300)))
+cv2.imshow(subjects[3], cv2.resize(predicted_img3, (300, 300)))
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.waitKey(1)
+cv2.destroyAllWindows()
